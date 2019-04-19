1e7f6771777f444d0853c37fc1a55ffc490a3b46
larryliu52 <46587094+larryliu52@users.noreply.github.com>
2019-04-18 23:05:15 -0700

    Add files via upload

--- InventorySearcher.java
+++ InventorySearcher.java
@@ -1,18 +1,22 @@
-package finalprojectgroup2;
+package InventorySearcher;
 
+import dto.Vehicle;
 import java.util.ArrayList;
 
 public interface InventorySearcher {
-
 	ArrayList<Vehicle> search_inventory(ArrayList<Vehicle> inventory,
-										String category,
-										String min_year,
-										String max_year,
-										String max_mileage,
-										String min_price,
-										String max_price,
-										String model,
-										String make,
-										String type,
-										String seat_count);
+													  String is_new,
+													  String is_used,
+													  String min_year,
+												      String max_year,
+													  String max_mileage,
+													  String min_price,
+													  String max_price,
+													  String model,
+													  String make,
+													  String type,
+													  String seat_count);
 }
+
+// use InventorySearcher.search_inventory(...) to get search result: return type: ArrayList<Vehicle>
+// put default value as null or "" if filters not be filled.
